69e91c4b6f65120915e12c74b4544e47820cb522
Patrik Fatura <patofatura@gmail.com>
2020-10-20 09:23:19 +0200

    Done

--- Open-Lab-02.05/Calculator.cs
+++ Open-Lab-02.05/Calculator.cs
@@ -9,7 +9,7 @@ namespace Open_Lab_02._05
            bool dudo = false;
             if(number % 5 == 0)
                 dudo = true;
-            return dudo;
+            return number;
         }
     }
 }
